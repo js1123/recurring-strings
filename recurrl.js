3ae753d5ed5588393b0e9048fcd0c9b89137e7ab
adresu <ad_resa@yahoo.com>
2017-04-21 23:14:34 +0300

    some cleanup, code styling

--- recurrl.js
+++ recurrl.js
@@ -5,44 +5,39 @@ function substitute() {
 	var resultDiv = document.createElement('div');
 	document.body.appendChild(resultDiv);
 	
-	var myValue = document.getElementById('myTextBox').value;
-	
+	var myValue = document.querySelector('#myTextBox').value;
 	findRecurringStrings(myValue);
 	
 	if (myValue.length == 0) {
 		alert('Please enter a real value in the text box!');
 		return;
 	}
-	var myTitle = document.getElementById('title');
-	myTitle.innerHTML = myValue;
-	//============	
-	//var randStr = "cheag12345677 12345689123 cheag klk cheag owdadf cheag sfsdd dddddd cheag dddddd dddddd dddddd dddddd";
+	var myTitle = document.querySelector('#title');
+	myTitle.textContent = myValue;
+	
 	var randStr = myValue;
 	var lungStr = randStr.length;
 	var lung = Math.floor(lungStr / 2);
-	console.log(randStr + "\xa0\xa0\xa0\xa0" +" Lungime = ", lungStr, " / ", lung);
+	console.log(randStr + "\xa0\xa0\xa0\xa0" + " Lungime = ", lungStr, " / ", lung);
 	do {	
 		var randStr1 = randStr;
-		for (i=0 ; i < lungStr - lung ; i++ ) {
+		for (i=0 ; i < lungStr - lung ; i++) {
 			k = 1
 			vil = randStr1.substr(i,lung);
-				for (j = i+1 + lung; j < lungStr ; j++)
+				for (j = i + 1 + lung; j < lungStr; j++)
 				{
-					if (randStr1.substr(j,lung) === vil ){
+					if (randStr1.substr(j,lung) === vil) {
 						k = k + 1;
 					}
 				}
-			if ( k > 1) { 
-				console.log(lung + " : " + "\xa0\xa0\xa0\xa0"+ vil + "\xa0\xa0\xa0\xa0 repetari: " + k);
+			if (k > 1) { 
+				console.log(lung + " : " + "\xa0\xa0\xa0\xa0" + vil + "\xa0\xa0\xa0\xa0 repetari: " + k);
+				
 				var resultLine = document.createElement('div');
-				resultLine.textContent = lung + " : " + "\xa0\xa0\xa0\xa0"+ vil + "\xa0\xa0\xa0\xa0 repetari: " + k;
+				resultLine.textContent = lung + " : " + "\xa0\xa0\xa0\xa0" + vil + "\xa0\xa0\xa0\xa0 repetari: " + k;
 				resultDiv.appendChild(resultLine);
-				
-				
 			}
 		}
-
-		//if (lung = 7) { debugger; }
 		lung = lung - 1;
 	} while (lung > 4)
 }
